2998987eeef8d7166a06207a6d623d0d652173d7
Xudoom <770188595@qq.com>
2022-09-11 19:15:34 +0800

    second commit

--- src/main/java/com/xudoom/git/GitTest.java
+++ src/main/java/com/xudoom/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xudoom.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
